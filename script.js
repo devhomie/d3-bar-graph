058f32d017a12e6295054dbcb93750c44788f4cc
devhomie <devhomie@proton.me>
2024-07-08 21:01:52 -0400

    Defined the - update - function
    Here we declared an -update- function, which gets called every time the text changes. The function
    creates, updates, or deletes the SVG elements needed to render the data, according to the same pattern
    we learned earlier for binding data to a selection (using the -data- method) and joining in the
    necessary elements with the simple version of the -join- method.
    
    The -join- method returns a selection containing all the current -rect- elements, including
    any that were just added. -- Each -rect- is now bound to a single datum, which represents a
    single character and the number of occurences of that character.
    
    We set the -width-, -height-, -x-, and -y- attributes as appropriate to create a horizontally
    oriented bar chart.
    
    The -width- attribute is set to 5 times the character count, so every new instance of a character
    makes the bar 5 pixel wider.
    
    The -height- attribute is a constant value of 10 (all bars are the same height), and the -x- attribute
    is a constant value of 20 ( all bars start 20 pixels from the left of the SVG element).
    
    The -y- attribute is set to 20 times the index of the datum, meaning that there will be a bar every
    20 pixels, giving 10 pixels of space between each bar.

--- script.js
+++ script.js
@@ -1,23 +1,36 @@
-
 const width = 600;
 const height = 600;
 
-
 // add an svg element to the page
-let svg = d3.select("body").append("svg").attr("width", width).attr("height", height);
+let svg = d3
+  .select("body")
+  .append("svg")
+  .attr("width", width)
+  .attr("height", height);
 
-d3.select("textarea").on("input", e =>{
-    let frequencies = {};
-    e.target.value.split("").forEach(char => {
-        let currentCount = frequencies[char] || 0;
-        frequencies[char] = currentCount + 1;
-    });
+function update(data) {
+  svg
+    .selectAll("rect")
+    .data(data)
+    .join("rect")
+    .attr("width", (d, i) => d.count * 5)
+    .attr("height", 10)
+    .attr("x", 20)
+    .attr("y", (d, i) => i * 20);
+}
 
-    let data = Object.entries(frequencies).map(pair => {
-        return { char: pair[0], count: pair[1]};
-    })
+d3.select("textarea").on("input", (e) => {
+  let frequencies = {};
+  e.target.value.split("").forEach((char) => {
+    let currentCount = frequencies[char] || 0;
+    frequencies[char] = currentCount + 1;
+  });
 
-    data.sort((a, b) => d3.ascending(a.char, b.char));
+  let data = Object.entries(frequencies).map((pair) => {
+    return { char: pair[0], count: pair[1] };
+  });
 
-    console.log(data)
-})
+  data.sort((a, b) => d3.ascending(a.char, b.char));
+
+  update(data);
+});
